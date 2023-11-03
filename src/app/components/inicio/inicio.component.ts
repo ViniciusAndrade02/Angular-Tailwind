2a16cc39595d3c68c86ccf63067c8db110a4a037
Vinicius Andrade <Viniciusjandrade08@gmail.com>
2023-11-03 04:31:08 -0300

    palheta de cores feito

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -7,9 +7,9 @@ import { Component, } from '@angular/core';
 })
 export class InicioComponent {
 
+  //brincando de aumnetar e diminuir
   valorDoInput!: string;
   numero:number = 10
-
 
   aumentar(){
     this.numero += 1
@@ -18,4 +18,37 @@ export class InicioComponent {
   diminuir(){
     this.numero -= 1
   }
+
+  //alterar a class4
+
+  classToApply: string = 'cinza';
+  classes: string[] =['cinza','vermelho','roxo']
+  NumeroArray: number = 0
+
+  alterarClass(){
+
+    if(this.NumeroArray != 2){
+      this.NumeroArray += 1
+    }else{
+      this.NumeroArray = 0
+    }
+   
+    this.classToApply = this.classes[this.NumeroArray]
+  }
+
+  //Desaparece a o botao alterar
+
+  alteracao:boolean = true
+  informacao:string = 'Aparecendo'
+
+  FuncaoAlterar(){
+    this.alteracao = !this.alteracao
+    if(this.alteracao == true){
+      this.informacao = 'Aparecendo'
+    }else{
+      this.informacao = 'Desapareceu'
+    }
+  }
+
+
 }
